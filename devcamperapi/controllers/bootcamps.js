386007109a4e33e32538983e3d012b6113dc2420
Rajat Kumar <991rajat@gmail.com>
2021-02-06 18:02:58 +0530

    added mongodb fun

--- devcamperapi/controllers/bootcamps.js
+++ devcamperapi/controllers/bootcamps.js
@@ -1,40 +1,97 @@
+const Bootcamp = require('../models/Bootcamps')
+
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req,res,next) => {
-    res.status(200).json({success:true,data:{
-        msg:'SHOW BOOTCAMPS'
-    }});
+exports.getBootcamps = async (req,res,next) => {
+    try{
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({
+            success:true,
+            count:bootcamps.length,
+            data:bootcamps
+        });
+    }catch(err){
+        res.status(400).json({
+            success:false,
+        });
+    }
 }
 
 
 // @desc    Get single bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req,res,next) => {
-    res.status(200).json({success:true,data:{
-        msg:`Get bootcamp ${req.params.id}`
-    }});
+exports.getBootcamp = async (req,res,next) => {
+   try{
+       
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if(!bootcamp){
+        return   res.status(400).json({
+            success:false,
+        });
+    }
+
+    res.status(200).json({
+        success:true,
+        data:bootcamp
+    });
+
+   }catch(err){
+    // res.status(400).json({
+    //         success:false,
+    //     });
+    next(err);
+   }
 }
 
 
 // @desc    Post single bootcamps
 // @route   POST /api/v1/bootcamps/
 // @access  Private
-exports.createBootcamp = (req,res,next) => {
-    res.status(200).json({success:true,data:{
-        msg:'CREATE BOOTCAMPS'
-    }});
-}
+exports.createBootcamp = async (req,res,next) => {
+    try{
+        const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        data:bootcamp
+    });
+    }catch(err){
+        res.status(400).json({
+            sucess:false,
+        })
+    }
+};
 
 
 // @desc    Update single bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-exports.updateBootcamp = (req,res,next) => {
-    res.status(200).json({success:true,data:{
-        msg:`Update bootcamp ${req.params.id}`
-    }});
+exports.updateBootcamp = async (req,res,next) => {
+    try{
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,runValidators:true
+    });
+
+    if(!bootcamp){
+        return res.status(400).json({
+            success:false
+        })
+    }
+
+    res.status(201).json({
+        success:true,
+        data:bootcamp
+    });
+    }
+    catch(err){
+        res.status(400).json({
+            sucess:false,
+        })
+    }
 }
 
 
@@ -42,10 +99,26 @@ exports.updateBootcamp = (req,res,next) => {
 // @desc    Delete single bootcamp
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
-exports.deleteBootcamp = (req,res,next) => {
-    res.status(200).json({success:true,data:{
-        msg:`Delete bootcamp ${req.params.id}`
-    }});
+exports.deleteBootcamp = async (req,res,next) => {
+    try{
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    
+        if(!bootcamp){
+            return res.status(400).json({
+                success:false
+            })
+        }
+    
+        res.status(201).json({
+            success:true,
+            data:{}
+        });
+        }
+        catch(err){
+            res.status(400).json({
+                sucess:false,
+            })
+        }
 }
 
 
--- devcamperapi/server.js
+++ devcamperapi/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 
 //ENV File
@@ -15,6 +16,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body Parse
+app.use(express.json());
+
 
 //Dev loggine middleware
 if(process.env.NODE_ENV === 'development'){
@@ -24,7 +28,7 @@ if(process.env.NODE_ENV === 'development'){
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps);
 
-
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 3000;
